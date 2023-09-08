f2c361b080445a193c51dfb90961225d73e1a4fa
maryam-hakim <111568487+maryam-hakim@users.noreply.github.com>
2023-09-08 02:52:25 -0700

    Update delete_comment.php

--- delete_comment.php
+++ delete_comment.php
@@ -10,6 +10,16 @@
 			if(isset($_GET['delete_comment'])){
 				
 				$delete_id_comment = $_GET['delete_comment'];
+
+				$sql = "SELECT * FROM `temp_comment` where `id-temp-comment` = '".$delete_id_comment."'";
+                $result = mysqli_query($conn, $sql);
+                 while($row=mysqli_fetch_array($result))
+                {
+                    $iduser = $row[1];
+                    $idbook = $row[2];
+                    $text = $row[3];
+					
+                }
 				
 				$delete_comment = "delete from temp_comment where `id-temp-comment`='$delete_id_comment' ";
 				
@@ -17,8 +27,13 @@
 				
 				if($run_delete_comment)
 				{
+					$Status = 1;
+					$_SESSION["idbook"] = $idbook;
+					$_SESSION["iduser"] = $iduser;
+					// $_SESSION["status"] = $Status;
 					
-					echo "<script>window.open('index.php?view_comment','_self')</script>";
+					
+					echo "<script>window.open('../Send_email.php?id=.$Status','_self')</script>";
 					
 				}
 			}
